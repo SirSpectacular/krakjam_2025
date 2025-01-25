33a8d9b0b1584c6364fc8eb4ab0bfffc9a0125c4
SirSpectacular <sszymek.5@gmail.com>
2025-01-25 21:39:25 +0100

    added utils script

---
+++ BubbleGuys/Assets/Scripts/Game/View/Player/MaintainWorldScale.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+namespace Game.View.Player
+{
+    public class MaintainWorldScale : MonoBehaviour
+    {
+        [Tooltip("The desired world scale for this object (X, Y, Z).")]
+        public Vector3 targetWorldScale = Vector3.one;
+
+        private void Awake()
+        {
+            targetWorldScale = transform.lossyScale;
+        }
+
+        void Update()
+        {
+            SetWorldScale(targetWorldScale);
+        }
+
+        /// <summary>
+        /// Adjusts the local scale of the object to match the desired world scale.
+        /// </summary>
+        /// <param name="desiredWorldScale">The desired scale in world space.</param>
+        private void SetWorldScale(Vector3 desiredWorldScale)
+        {
+            if (transform.parent != null)
+            {
+                // Calculate the local scale needed to achieve the desired world scale
+                Vector3 parentScale = transform.parent.lossyScale;
+                transform.localScale = new Vector3(
+                    desiredWorldScale.x / parentScale.x,
+                    desiredWorldScale.y / parentScale.y,
+                    desiredWorldScale.z / parentScale.z
+                );
+            }
+            else
+            {
+                // If no parent, simply set the local scale to the desired world scale
+                transform.localScale = desiredWorldScale;
+            }
+        }
+    }
+}
