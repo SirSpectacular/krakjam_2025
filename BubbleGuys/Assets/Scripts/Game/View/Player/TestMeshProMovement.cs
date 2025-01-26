11ef29165f0f3972ecd2ce58f1ba5bb59f09f17b
LucasJezap <bossgame32@gmail.com>
2025-01-26 11:12:24 +0100

    server-client changes etc

--- BubbleGuys/Assets/Scripts/Game/View/Player/TestMeshProMovement.cs
+++ BubbleGuys/Assets/Scripts/Game/View/Player/TestMeshProMovement.cs
@@ -4,6 +4,7 @@ namespace Game.View.Player
 {
     public class TestMeshProMovement : MonoBehaviour
     {
+        [SerializeField] private float _yOffset = 2;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -13,7 +14,8 @@ namespace Game.View.Player
         // Update is called once per frame
         void Update()
         {
-            transform.rotation = Quaternion.Euler(0.0f, 0.0f, transform.parent.rotation.z * -1.0f);
+            transform.position = transform.parent.position + Vector3.up * _yOffset;
+            transform.rotation = Quaternion.identity;
         }
     }
 }
